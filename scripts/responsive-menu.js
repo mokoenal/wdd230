9c9043b879dca3a3daef9f87d2c3f3a3539056f4
mokoenal <lindiwemokoena374@gmail.com>
2024-01-27 15:02:21 +0200

    added to home page

--- scripts/responsive-menu.js
+++ scripts/responsive-menu.js
@@ -1,7 +1,7 @@
 const hamButton = document.querySelector('#menu');
-const mainnav = document.querySelector('.navigation');
+const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
-    mainnav.classList.toggle('open');
+    navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
