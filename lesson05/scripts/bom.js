599f2aedd6a5fc3de50a0765ee66817e1874f490
mokoenal <lindiwemokoena374@gmail.com>
2024-02-10 10:14:01 +0200

    add to the bom

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,40 @@
+
+// elements to link to the document
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+//event listener for the button
+button.addEventListener('click', () => {
+
+    // check if the input value is not empty
+    if (input.value != '') {
+
+        // create a new list item and a delete button
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        // add the input value to the list item and the delete // button to the list item
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+        input.value = ''; // clear the input value 
+
+        // add an event listener for the delete button
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+            input.value = '';
+        });
+
+        input.focus();
+    }
+});
+
+// add an event listener for the input to allow the user to Add entrys with "Enter" key
+input.addEventListener('keyup', (e) => {
+    if (e.code === 'Enter') {
+        button.click();
+    }
+});
