1005849174eec62d426114400473f25e55800315
mokoenal <lindiwemokoena374@gmail.com>
2024-02-10 21:28:55 +0200

    added to the chamber website

---
+++ chamber/scripts/darkmode.js
@@ -0,0 +1,17 @@
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+const nav = document.querySelector(".navigation");
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        body.style.background = "#000";
+        body.style.color = "#fff";
+        nav.style.border = "2px solid black";
+        modeButton.textContent = "🔆";
+    } else {
+        body.style.background = "#eee";
+        body.style.color = "#000";
+        nav.style.border = "2px solid #FEFEFE";
+        modeButton.textContent = "🕶️";
+    }
+});
