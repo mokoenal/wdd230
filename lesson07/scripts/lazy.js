191560d7e1a070ca31ad3cb96118909e8b8a8c26
mokoenal <lindiwemokoena374@gmail.com>
2024-02-20 21:12:35 +0200

    added on the lazyload

---
+++ lesson07/scripts/lazy.js
@@ -0,0 +1,5 @@
+// insert copyright year
+document.getElementById('yearDate').textContent = new Date().getFullYear();
+
+// Display last modified date
+document.getElementById('lastModified').textContent = document.lastModified;
